05193b0c69660caea71040dacac2168b9b264685
haotianmingyue <2421912570@qq.coom>
2021-11-17 16:58:44 +0800

    11.17 排列个数动态规划完成

--- 09排列个数.py
+++ 09排列个数.py
@@ -5,23 +5,34 @@
 # @file 09排列个数.py
 class Solution:
     def combinationSum4(self, nums: list[int], target: int) -> int:
-        c :int = 0
-        ans = list()
-        t_l = list()
-        def find_all(t_n,t_s):
-            if t_n == 0 and t_s == target:
-                if t_l not in ans:
-                    ans.append(t_l[:])
-            elif t_n > 0 and t_s < target:
+        import collections
+        d = collections.defaultdict(int)
+        c: int = 0
+        def find_all(n):
+            nonlocal d
+            nonlocal c
+            if n == 0:
+                c += 1
+                return 1
+            elif n > 0:
+                t: int = 0
                 for i in range(len(nums)):
-                    t_l.append(nums[i])
-                    find_all(t_n-1,t_s+nums[i])
-                    t_l.pop()
-        for i in range(1,target//min(nums)+1):
-            find_all(i,0)
-        print(ans)
-        return len(ans)
+                    if n - nums[i] in d:
+                        c += d[n - nums[i]]
+                        t += d[n - nums[i]]
+                    else:
+                        t += find_all(n - nums[i])
+                d[n] = t
+                return t
+            else:
+                return 0
+
+        find_all(target)
+        print(d)
+        return c
 
 if __name__ == '__main__':
     s = Solution()
-    print(s.combinationSum4([1,2,3,5,4,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30],40))
+    print(s.combinationSum4([1,2,3,5,4,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30],20))
+    # print(2**32)
+    # print(s.combinationSum4([1, 2, 3],4))
